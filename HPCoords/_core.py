69cbeff25eb2d118c028960370ad1435c36e9dce
tytskiy <vlad75.76@mail.ru>
2020-11-09 12:43:01 +0300

    technical commit

---
+++ HPCoords/_core.py
@@ -0,0 +1,23 @@
+def f(a):
+    """[summary]
+
+    Parameters
+    ----------
+    a : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+
+    Raises
+    ------
+    TypeError
+        [description]
+    """    
+    if(a is None):
+        raise TypeError
+    return 1
+
+  
