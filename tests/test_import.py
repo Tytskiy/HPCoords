ba16d12ffd2a4c0f076b8a6276969ce7120e3828
tytskiy <vlad75.76@mail.ru>
2021-03-05 20:13:30 +0300

    clearing and moving stuff

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,2 +1,30 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+
 from hpcoords import parallel_coordinates
-print(parallel_coordinates)
+
+#print(parallel_coordinates)
+
+iris = sns.load_dataset('iris')
+
+plt.style.use("seaborn")
+
+mpl.rcParams["axes.linewidth"] = 1
+mpl.rcParams["axes.edgecolor"] = "grey"
+mpl.rcParams["ytick.labelsize"] = 12
+mpl.rcParams["xtick.labelsize"] = 14
+
+parallel_coordinates(
+    iris,
+    hue="species",
+    spline=True,
+    directions=[1, 1, 1, 1],
+    bundling_coef=0.1,
+    lw=1.5,
+    alpha=0.3,
+    figsize=(14, 7)
+)
+plt.show()
